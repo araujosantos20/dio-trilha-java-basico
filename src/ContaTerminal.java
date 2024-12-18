c957bb08fbbb2031c734214acebb37b613bd9b6d
Mateus Santos <mateus.sanjo20@gmail.com>
2024-12-18 17:18:19 -0300

    Utilização do Scanner para interação da conta bancária com o usuário e mensagem após a passagem de dados informadas pelo usuário

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner usuario = new Scanner(System.in);
+
+        System.out.println("Digite a agência do seu banco: ");
+        String agencia = usuario.nextLine();
+        
+        System.out.println("Digite o número do seu banco: ");
+        int numero = usuario.nextInt();
+        usuario.nextLine();
+
+        System.out.println("Digite o seu nome: ");
+        String nomeCliente = usuario.nextLine().toUpperCase();
+
+        System.out.println("Digite seu saldo atual: ");
+        Double saldo = usuario.nextDouble();
+
+        usuario.close();
+
+        System.out.println("Olá " + nomeCliente + " obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo de " + saldo + " já está disponível para saque!");
     }
 }
